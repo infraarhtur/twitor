481c7f09aa2d4a346d4f6552237b48db41f0097b
Arhtur monsalve <luismonsalve@cbit-online.com>
2019-12-05 17:27:22 -0500

    subimos version del cache

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dinamic-v2';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dinamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
